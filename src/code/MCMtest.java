6ff590470be0cc492911b7bd097fe65bbdb9b4a0
Jorge Rivas <jorge2233@hotmail.es>
2019-03-15 20:57:09 +0100

    clases de prueba

---
+++ src/code/MCMtest.java
@@ -0,0 +1,36 @@
+package code;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MCMtest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		MinimoComunMultiplo m= new MinimoComunMultiplo();
+		int resultado= m.candidatosMcM(null);
+		assertEquals(null,resultado);
+	}
+
+}
